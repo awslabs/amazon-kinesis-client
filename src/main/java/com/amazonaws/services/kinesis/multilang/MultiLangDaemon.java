66cbcba33a685e7dad2d2910b39cfcaa18393467
Ikram ulhaq <iulhaq@lyft.com>
2017-06-05 10:24:52 -0700

    Shutdown requested via dispatch.

--- src/main/java/com/amazonaws/services/kinesis/multilang/MultiLangDaemon.java
+++ src/main/java/com/amazonaws/services/kinesis/multilang/MultiLangDaemon.java
@@ -16,10 +16,13 @@ package com.amazonaws.services.kinesis.multilang;
 
 import java.io.IOException;
 import java.io.PrintStream;
+
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -145,17 +148,16 @@ public class MultiLangDaemon implements Callable<Integer> {
                 config.getRecordProcessorFactory(),
                 executorService);
 
-        Runtime.getRuntime().addShutdownHook(new Thread()
-        {
+        Runtime.getRuntime().addShutdownHook(new Thread() {
             @Override
             public void run()
             {
                 LOG.info("Process terminanted, will initiate shutdown.");
                 try {
                     Future<Void> fut = daemon.worker.requestShutdown();
-                    fut.get();
+                    fut.get(5000, TimeUnit.MILLISECONDS);
                     LOG.info("Process shutdown is complete.");
-                } catch (InterruptedException | ExecutionException e) {
+                } catch (InterruptedException | ExecutionException | TimeoutException e) {
                     LOG.error("Encountered an error during shutdown.", e);
                 }
             }
---
+++ src/main/java/com/amazonaws/services/kinesis/multilang/messages/ShutdownRequestedMessage.java
@@ -0,0 +1,17 @@
+package com.amazonaws.services.kinesis.multilang.messages;
+
+/**
+ * A message to indicate to the client's process that shutdown is requested.
+ */
+public class ShutdownRequestedMessage extends Message {
+    /**
+     * The name used for the action field in {@link Message}.
+     */
+    public static final String ACTION = "shutdownrequested";
+
+    /**
+     * Convenience constructor.
+     */
+    public ShutdownRequestedMessage() {
+    }
+}
