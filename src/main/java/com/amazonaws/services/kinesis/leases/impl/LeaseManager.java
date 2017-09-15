656b17ceaabd511da5f12cf223468a7e81f48407
Sahil Palvia <spalvia1990@gmail.com>
2017-09-15 11:04:30 -0700

    Adding logging for DynamoDB ProvisionedThroughputExceededExcpetion (#212)
    * Adding logging to ProvisionedThroughputExceededException.
    
    * Addressing CR comments and changing log level from error to warn
    
    * Updated the comments as per cr comments.

--- src/main/java/com/amazonaws/services/kinesis/leases/impl/LeaseManager.java
+++ src/main/java/com/amazonaws/services/kinesis/leases/impl/LeaseManager.java
@@ -564,6 +564,7 @@ public class LeaseManager<T extends Lease> implements ILeaseManager<T> {
     protected DependencyException convertAndRethrowExceptions(String operation, String leaseKey, AmazonClientException e)
         throws ProvisionedThroughputException, InvalidStateException {
         if (e instanceof ProvisionedThroughputExceededException) {
+            LOG.warn("Provisioned Throughput on the lease table has been exceeded. It's recommended that you increase the IOPs on the table. Failure to increase the IOPs may cause the application to not make progress.");
             throw new ProvisionedThroughputException(e);
         } else if (e instanceof ResourceNotFoundException) {
             // @formatter:on
