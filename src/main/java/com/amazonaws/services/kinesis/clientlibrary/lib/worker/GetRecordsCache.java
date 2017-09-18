c92a5b556c7d835602c7921c58bbeafb4c773b8c
Sahil Palvia <spalvia1990@gmail.com>
2017-09-18 16:35:31 -0700

    Adding the cache and the retriver stubs. (#215)
    * Adding the cache and the retriver stubs.
    
    * Addressing comments and adding initial documentation and changing the retreiver from interface to class.
    
    * Reverting back to the interface
    
    * Fixing minor error
    
    * Adding default cache stub

---
+++ src/main/java/com/amazonaws/services/kinesis/clientlibrary/lib/worker/GetRecordsCache.java
@@ -0,0 +1,18 @@
+package com.amazonaws.services.kinesis.clientlibrary.lib.worker;
+
+import com.amazonaws.services.kinesis.model.GetRecordsResult;
+
+/**
+ * This class is used as a cache for Prefetching data from Kinesis.
+ */
+public interface GetRecordsCache {
+    /**
+     * This method returns the next set of records from the Cache if present, or blocks the request till it gets the
+     * next set of records back from Kinesis.
+     * 
+     * @return The next set of records.
+     */
+    GetRecordsResult getNextResult();
+    
+    void shutdown();
+}
