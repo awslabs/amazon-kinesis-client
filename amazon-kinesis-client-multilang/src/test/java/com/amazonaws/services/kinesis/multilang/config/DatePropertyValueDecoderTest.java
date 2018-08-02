978fe2671eb811c3a1dd8193e49f0cfd0e6ef46b
Pfifer, Justin <pfifer@amazon.com>
2018-08-02 12:32:44 -0700

    Cleanup a merge artifact

--- amazon-kinesis-client-multilang/src/test/java/com/amazonaws/services/kinesis/multilang/config/DatePropertyValueDecoderTest.java
+++ amazon-kinesis-client-multilang/src/test/java/com/amazonaws/services/kinesis/multilang/config/DatePropertyValueDecoderTest.java
@@ -12,16 +12,13 @@
  * express or implied. See the License for the specific language governing
  * permissions and limitations under the License.
  */
-package com.amazonaws.services.kinesis.clientlibrary.config;
+package com.amazonaws.services.kinesis.multilang.config;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
 
 import java.util.Date;
 
 import org.junit.Test;
-
-import com.amazonaws.services.kinesis.clientlibrary.config.DatePropertyValueDecoder;
 
 public class DatePropertyValueDecoderTest {
 
