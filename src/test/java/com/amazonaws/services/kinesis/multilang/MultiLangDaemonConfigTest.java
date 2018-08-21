15ede3c507c807ac9b6bdf4943050acf174ce4e1
Michael Scharp <michael.scharp@fmr.com>
2018-08-21 10:23:32 -0600

    Add proxy config unit tests

--- src/test/java/com/amazonaws/services/kinesis/multilang/MultiLangDaemonConfigTest.java
+++ src/test/java/com/amazonaws/services/kinesis/multilang/MultiLangDaemonConfigTest.java
@@ -18,6 +18,10 @@ import static org.junit.Assert.assertNotNull;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.lang.reflect.Field;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
 import junit.framework.Assert;
@@ -39,23 +43,37 @@ public class MultiLangDaemonConfigTest {
         AWSCredentials creds = Mockito.mock(AWSCredentials.class);
         Mockito.doReturn(creds).when(credentialsProvider).getCredentials();
         Mockito.doReturn("cool-user").when(creds).getAWSAccessKeyId();
-        KinesisClientLibConfiguration kclConfig =
-                new KinesisClientLibConfiguration("cool-app", "cool-stream", credentialsProvider, "cool-worker");
+        KinesisClientLibConfiguration kclConfig = new KinesisClientLibConfiguration("cool-app", "cool-stream",
+                credentialsProvider, "cool-worker");
         KinesisClientLibConfigurator configurator = Mockito.mock(KinesisClientLibConfigurator.class);
         Mockito.doReturn(kclConfig).when(configurator).getConfiguration(Mockito.any(Properties.class));
         return configurator;
     }
 
-    @Test
-    public void constructorTest() throws IOException {
+    // Function to mock ENV variables
+    public static void setEnv(Map<String, String> newenv) throws Exception {
+        Class[] classes = Collections.class.getDeclaredClasses();
+        Map<String, String> env = System.getenv();
+        for (Class cl : classes) {
+            if ("java.util.Collections$UnmodifiableMap".equals(cl.getName())) {
+                Field field = cl.getDeclaredField("m");
+                field.setAccessible(true);
+                Object obj = field.get(env);
+                Map<String, String> map = (Map<String, String>) obj;
+                map.clear();
+                map.putAll(newenv);
+            }
+        }
+    }
+
+    @Test public void constructorTest() throws IOException {
         String PROPERTIES =
                 "executableName = randomEXE \n" + "applicationName = testApp \n" + "streamName = fakeStream \n"
                         + "AWSCredentialsProvider = DefaultAWSCredentialsProviderChain\n"
                         + "processingLanguage = malbolge";
         ClassLoader classLoader = Mockito.mock(ClassLoader.class);
 
-        Mockito.doReturn(new ByteArrayInputStream(PROPERTIES.getBytes()))
-                .when(classLoader)
+        Mockito.doReturn(new ByteArrayInputStream(PROPERTIES.getBytes())).when(classLoader)
                 .getResourceAsStream(FILENAME);
 
         MultiLangDaemonConfig deamonConfig = new MultiLangDaemonConfig(FILENAME, classLoader, buildMockConfigurator());
@@ -65,16 +83,12 @@ public class MultiLangDaemonConfigTest {
         assertNotNull(deamonConfig.getRecordProcessorFactory());
     }
 
-    @Test
-    public void propertyValidation() {
-        String PROPERTIES_NO_EXECUTABLE_NAME =
-                "applicationName = testApp \n" + "streamName = fakeStream \n"
-                        + "AWSCredentialsProvider = DefaultAWSCredentialsProviderChain\n"
-                        + "processingLanguage = malbolge";
+    @Test public void propertyValidation() {
+        String PROPERTIES_NO_EXECUTABLE_NAME = "applicationName = testApp \n" + "streamName = fakeStream \n"
+                + "AWSCredentialsProvider = DefaultAWSCredentialsProviderChain\n" + "processingLanguage = malbolge";
         ClassLoader classLoader = Mockito.mock(ClassLoader.class);
 
-        Mockito.doReturn(new ByteArrayInputStream(PROPERTIES_NO_EXECUTABLE_NAME.getBytes()))
-                .when(classLoader)
+        Mockito.doReturn(new ByteArrayInputStream(PROPERTIES_NO_EXECUTABLE_NAME.getBytes())).when(classLoader)
                 .getResourceAsStream(FILENAME);
 
         MultiLangDaemonConfig config;
@@ -88,4 +102,126 @@ public class MultiLangDaemonConfigTest {
         }
     }
 
+    @Test public void KinesisClientLibConfiguration_should_get_proxy_info_from_properties_file() {
+        String PROPERTIES =
+                "executableName = randomEXE \n" + "applicationName = testApp \n" + "streamName = fakeStream \n"
+                        + "AWSCredentialsProvider = DefaultAWSCredentialsProviderChain\n"
+                        + "http.proxyHost = http://proxy.com\n" + "http.proxyPort = 1234\n"
+                        + "processingLanguage = malbolge";
+        ClassLoader classLoader = Mockito.mock(ClassLoader.class);
+
+        Mockito.doReturn(new ByteArrayInputStream(PROPERTIES.getBytes())).when(classLoader)
+                .getResourceAsStream(FILENAME);
+
+        MultiLangDaemonConfig config;
+
+        try {
+            config = new MultiLangDaemonConfig(FILENAME, classLoader, buildMockConfigurator());
+            assertAgainstKclConfig(config.getKinesisClientLibConfiguration(), "http://proxy.com", 1234);
+        } catch (IllegalArgumentException e) {
+            // Bad
+        } catch (IOException e) {
+            Assert.fail();
+        }
+    }
+
+    @Test public void KinesisClientLibConfiguration_should_get_proxy_info_from_system_properties() {
+        String PROPERTIES =
+                "executableName = randomEXE \n" + "applicationName = testApp \n" + "streamName = fakeStream \n"
+                        + "AWSCredentialsProvider = DefaultAWSCredentialsProviderChain\n"
+                        + "processingLanguage = malbolge";
+        ClassLoader classLoader = Mockito.mock(ClassLoader.class);
+
+        Mockito.doReturn(new ByteArrayInputStream(PROPERTIES.getBytes())).when(classLoader)
+                .getResourceAsStream(FILENAME);
+
+        MultiLangDaemonConfig config;
+
+        System.setProperty("http.proxyHost", "http://proxy.com");
+        System.setProperty("http.proxyPort", "1234");
+
+        try {
+            config = new MultiLangDaemonConfig(FILENAME, classLoader, buildMockConfigurator());
+            assertAgainstKclConfig(config.getKinesisClientLibConfiguration(), "http://proxy.com", 1234);
+        } catch (IllegalArgumentException e) {
+            // Bad
+        } catch (IOException e) {
+            Assert.fail();
+        }
+    }
+
+    @Test public void KinesisClientLibConfiguration_should_get_proxy_info_from_env_vars() {
+        String PROPERTIES =
+                "executableName = randomEXE \n" + "applicationName = testApp \n" + "streamName = fakeStream \n"
+                        + "AWSCredentialsProvider = DefaultAWSCredentialsProviderChain\n"
+                        + "processingLanguage = malbolge";
+        ClassLoader classLoader = Mockito.mock(ClassLoader.class);
+
+        Map<String, String> env = new HashMap<>();
+        env.put("http_proxy", "http://proxy.com:1234");
+
+        try {
+            setEnv(env);
+
+            Mockito.doReturn(new ByteArrayInputStream(PROPERTIES.getBytes())).when(classLoader)
+                    .getResourceAsStream(FILENAME);
+
+            MultiLangDaemonConfig config;
+
+            try {
+                config = new MultiLangDaemonConfig(FILENAME, classLoader, buildMockConfigurator());
+
+                // host doesn't have protocol here because URI splits that into a separate piece
+                assertAgainstKclConfig(config.getKinesisClientLibConfiguration(), "proxy.com", 1234);
+            } catch (IllegalArgumentException e) {
+                // Bad
+            } catch (IOException e) {
+                Assert.fail();
+            }
+        } catch (Exception e) {
+            Assert.fail();
+        }
+    }
+
+    @Test public void KinesisClientLibConfiguration_should_not_get_proxy_info() {
+        String PROPERTIES =
+                "executableName = randomEXE \n" + "applicationName = testApp \n" + "streamName = fakeStream \n"
+                        + "AWSCredentialsProvider = DefaultAWSCredentialsProviderChain\n"
+                        + "processingLanguage = malbolge";
+        ClassLoader classLoader = Mockito.mock(ClassLoader.class);
+
+        Map<String, String> env = new HashMap<>();
+
+        try {
+            //clear out any env vars loaded from system
+            setEnv(env);
+
+            Mockito.doReturn(new ByteArrayInputStream(PROPERTIES.getBytes())).when(classLoader)
+                    .getResourceAsStream(FILENAME);
+
+            MultiLangDaemonConfig config;
+
+            try {
+                config = new MultiLangDaemonConfig(FILENAME, classLoader, buildMockConfigurator());
+
+                // host doesn't have protocol here because URI splits that into a separate piece
+                assertAgainstKclConfig(config.getKinesisClientLibConfiguration(), null, -1);
+            } catch (IllegalArgumentException e) {
+                // Bad
+            } catch (IOException e) {
+                Assert.fail();
+            }
+        } catch (Exception e) {
+            Assert.fail();
+        }
+    }
+
+    private void assertAgainstKclConfig(KinesisClientLibConfiguration kclConfig, String host, int port) {
+        Assert.assertEquals(host, kclConfig.getKinesisClientConfiguration().getProxyHost());
+        Assert.assertEquals(host, kclConfig.getDynamoDBClientConfiguration().getProxyHost());
+        Assert.assertEquals(host, kclConfig.getCloudWatchClientConfiguration().getProxyHost());
+        Assert.assertEquals(port, kclConfig.getKinesisClientConfiguration().getProxyPort());
+        Assert.assertEquals(port, kclConfig.getDynamoDBClientConfiguration().getProxyPort());
+        Assert.assertEquals(port, kclConfig.getCloudWatchClientConfiguration().getProxyPort());
+    }
 }
