9ae07284215d9fe4d55fea367ffa79566411f2da
Ashwin Giridharan <girida@amazon.com>
2019-08-08 13:37:29 -0700

    Removing cloudwatch fix. Will be released in a separate commit.

--- amazon-kinesis-client/src/main/java/software/amazon/kinesis/metrics/CloudWatchMetricsPublisher.java
+++ amazon-kinesis-client/src/main/java/software/amazon/kinesis/metrics/CloudWatchMetricsPublisher.java
@@ -14,15 +14,14 @@
  */
 package software.amazon.kinesis.metrics;
 
-import lombok.extern.slf4j.Slf4j;
-import software.amazon.awssdk.services.cloudwatch.CloudWatchAsyncClient;
-import software.amazon.awssdk.services.cloudwatch.model.MetricDatum;
-import software.amazon.awssdk.services.cloudwatch.model.PutMetricDataRequest;
-
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
+
+import lombok.extern.slf4j.Slf4j;
+import software.amazon.awssdk.services.cloudwatch.CloudWatchAsyncClient;
+import software.amazon.awssdk.services.cloudwatch.model.CloudWatchException;
+import software.amazon.awssdk.services.cloudwatch.model.MetricDatum;
+import software.amazon.awssdk.services.cloudwatch.model.PutMetricDataRequest;
 
 /**
  * Publisher that contains the logic to publish metrics.
@@ -31,7 +30,6 @@ import java.util.concurrent.TimeUnit;
 public class CloudWatchMetricsPublisher {
     // CloudWatch API has a limit of 20 MetricDatums per request
     private static final int BATCH_SIZE = 20;
-    private static final int PUT_TIMEOUT_MILLIS = 5000;
 
     private final String namespace;
     private final CloudWatchAsyncClient cloudWatchClient;
@@ -62,11 +60,11 @@ public class CloudWatchMetricsPublisher {
             request = request.metricData(metricData);
 
             try {
-                // This needs to be blocking. Making it asynchronous leads to increased throttling.
-                cloudWatchClient.putMetricData(request.build()).get(PUT_TIMEOUT_MILLIS, TimeUnit.MILLISECONDS);
-            } catch (Exception e) {
-                log.warn("Could not publish {} datums to CloudWatch", endIndex - startIndex,
-                        e instanceof ExecutionException ? e.getCause() : e);
+                cloudWatchClient.putMetricData(request.build());
+
+                log.debug("Successfully published {} datums.", endIndex - startIndex);
+            } catch (CloudWatchException e) {
+                log.warn("Could not publish {} datums to CloudWatch", endIndex - startIndex, e);
             }
         }
     }
