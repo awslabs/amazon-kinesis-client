2f6e5d32c14d4f1adcb95e8887b086fbc5033c56
jushkem <20001595+jushkem@users.noreply.github.com>
2020-02-10 17:44:44 -0800

    Removing unused imports.

--- amazon-kinesis-client/src/main/java/software/amazon/kinesis/common/RequestDetails.java
+++ amazon-kinesis-client/src/main/java/software/amazon/kinesis/common/RequestDetails.java
@@ -3,10 +3,6 @@ package software.amazon.kinesis.common;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.experimental.Accessors;
-import software.amazon.awssdk.services.kinesis.model.KinesisResponseMetadata;
-
-import java.time.Instant;
-import java.util.Optional;
 
 @Accessors(fluent = true)
 @Getter
