c5ce30f6f7188f2e759f3c398ff1c7d2a6c3d553
Ravindranath Kakarla <ravindhranath@gmail.com>
2021-09-30 05:05:44 -0700

    Emit Glue Schema Registry usage metrics (#855)
    * Emit Glue Schema Registry usage metrics
    
    * Append KCL version to the app string.
    
    Co-authored-by: Ravindranath Kakarla <rnath@amazon.com>

--- amazon-kinesis-client/src/main/java/software/amazon/kinesis/schemaregistry/SchemaRegistryDecoder.java
+++ amazon-kinesis-client/src/main/java/software/amazon/kinesis/schemaregistry/SchemaRegistryDecoder.java
@@ -3,6 +3,7 @@ package software.amazon.kinesis.schemaregistry;
 import com.amazonaws.services.schemaregistry.common.Schema;
 import com.amazonaws.services.schemaregistry.deserializers.GlueSchemaRegistryDeserializer;
 import lombok.extern.slf4j.Slf4j;
+import software.amazon.kinesis.common.KinesisClientLibraryPackage;
 import software.amazon.kinesis.retrieval.KinesisClientRecord;
 
 import java.nio.ByteBuffer;
@@ -14,11 +15,13 @@ import java.util.List;
  */
 @Slf4j
 public class SchemaRegistryDecoder {
+    private static final String USER_AGENT_APP_NAME = "kcl" + "-" + KinesisClientLibraryPackage.VERSION;
     private final GlueSchemaRegistryDeserializer glueSchemaRegistryDeserializer;
 
     public SchemaRegistryDecoder(
         GlueSchemaRegistryDeserializer glueSchemaRegistryDeserializer) {
         this.glueSchemaRegistryDeserializer = glueSchemaRegistryDeserializer;
+        this.glueSchemaRegistryDeserializer.overrideUserAgentApp(USER_AGENT_APP_NAME);
     }
 
     /**
