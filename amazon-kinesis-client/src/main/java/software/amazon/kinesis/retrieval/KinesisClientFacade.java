4c4708ac09e22c50103e8fbc45047b4a0fd8c41b
stair <stairdav@amazon.com>
2023-04-18 16:53:34 -0400

    StreamARN: enhanced `StreamIdentifier` to spawn instances from stream ARNs.

---
+++ amazon-kinesis-client/src/main/java/software/amazon/kinesis/retrieval/KinesisClientFacade.java
@@ -0,0 +1,103 @@
+package software.amazon.kinesis.retrieval;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+import software.amazon.awssdk.services.kinesis.KinesisAsyncClient;
+import software.amazon.awssdk.services.kinesis.model.DescribeStreamSummaryRequest;
+import software.amazon.awssdk.services.kinesis.model.DescribeStreamSummaryResponse;
+import software.amazon.awssdk.services.kinesis.model.KinesisException;
+import software.amazon.awssdk.services.kinesis.model.LimitExceededException;
+import software.amazon.awssdk.services.kinesis.model.ResourceInUseException;
+import software.amazon.awssdk.services.kinesis.model.ResourceNotFoundException;
+import software.amazon.kinesis.common.KinesisRequestsBuilder;
+
+/**
+ * Facade pattern to simplify interactions with a {@link KinesisAsyncClient}.
+ */
+@Slf4j
+public final class KinesisClientFacade {
+
+    /**
+     * Reusable {@link AWSExceptionManager}.
+     * <p>
+     * N.B. This instance is mutable, but thread-safe for <b>read-only</b> use.
+     * </p>
+     */
+    private static final AWSExceptionManager AWS_EXCEPTION_MANAGER;
+
+    // FIXME dependency injection
+    private static KinesisAsyncClient kinesisClient;
+
+    static {
+        AWS_EXCEPTION_MANAGER = new AWSExceptionManager();
+        AWS_EXCEPTION_MANAGER.add(KinesisException.class, t -> t);
+        AWS_EXCEPTION_MANAGER.add(LimitExceededException.class, t -> t);
+        AWS_EXCEPTION_MANAGER.add(ResourceInUseException.class, t -> t);
+        AWS_EXCEPTION_MANAGER.add(ResourceNotFoundException.class, t -> t);
+    }
+
+    static void initialize(final KinesisAsyncClient client) {
+        kinesisClient = client;
+    }
+
+    public static DescribeStreamSummaryResponse describeStreamSummary(final String streamArn) {
+        final DescribeStreamSummaryRequest request = KinesisRequestsBuilder
+                .describeStreamSummaryRequestBuilder().streamARN(streamArn).build();
+        final CompletableFuture<DescribeStreamSummaryResponse> future = kinesisClient.describeStreamSummary(request);
+        final ServiceCallerSupplier<DescribeStreamSummaryResponse> dss =
+                () -> future.get(10, TimeUnit.SECONDS);
+        return retryWhenThrottled(dss, 3, streamArn, "DescribeStreamSummary");
+    }
+
+    // FIXME code lifted-and-shifted from FanOutConsumerRegistration; that class
+    //      (and others) should not be responsible for interacting directly with
+    //      the thread-safe Kinesis client (and handling retries, etc.)
+    private static <T> T retryWhenThrottled(
+            @NonNull final ServiceCallerSupplier<T> retriever,
+            final int maxRetries,
+            final String streamArn,
+            @NonNull final String apiName) {
+        LimitExceededException finalException = null;
+
+        int retries = maxRetries;
+        while (retries > 0) {
+            try {
+                try {
+                    return retriever.get();
+                } catch (ExecutionException e) {
+                    throw AWS_EXCEPTION_MANAGER.apply(e.getCause());
+                } catch (InterruptedException e) {
+                    throw KinesisException.create("Unable to complete " + apiName, e);
+                } catch (TimeoutException te) {
+                    log.info("Timed out waiting for " + apiName + " for " + streamArn);
+                }
+            } catch (LimitExceededException e) {
+                log.info("{} : Throttled while calling {} API, will backoff.", streamArn, apiName);
+                try {
+                    Thread.sleep(1000 + (long) (Math.random() * 100));
+                } catch (InterruptedException ie) {
+                    log.debug("Sleep interrupted, shutdown invoked.");
+                }
+                finalException = e;
+            }
+            retries--;
+        }
+
+        if (finalException == null) {
+            throw new IllegalStateException(streamArn + " : Exhausted retries while calling " + apiName);
+        }
+
+        throw finalException;
+    }
+
+    @FunctionalInterface
+    private interface ServiceCallerSupplier<T> {
+        T get() throws ExecutionException, InterruptedException, TimeoutException;
+    }
+
+}
--- amazon-kinesis-client/src/test/java/software/amazon/kinesis/common/StreamIdentifierTest.java
+++ amazon-kinesis-client/src/test/java/software/amazon/kinesis/common/StreamIdentifierTest.java
@@ -1,5 +1,6 @@
 package software.amazon.kinesis.common;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,36 +9,111 @@ import org.powermock.modules.junit4.PowerMockRunner;
 import software.amazon.awssdk.arns.Arn;
 import software.amazon.awssdk.regions.Region;
 
+import java.util.Arrays;
 import java.util.Optional;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertNotNull;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyString;
-import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.when;
 import static org.powermock.api.mockito.PowerMockito.mockStatic;
+import static org.powermock.api.mockito.PowerMockito.verifyStatic;
+import static software.amazon.kinesis.common.StreamARNUtil.getStreamARN;
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest(StreamARNUtil.class)
 public class StreamIdentifierTest {
-    private static final String STREAM_NAME = "streamName";
+    private static final String STREAM_NAME = "stream-name";
     private static final Region KINESIS_REGION = Region.US_WEST_1;
-    private static final String TEST_ACCOUNT_ID = "111111111111";
-    private static final String EPOCH = "1680616058";
+    private static final String TEST_ACCOUNT_ID = "123456789012";
+    private static final long EPOCH = 1680616058L;
 
-    private static final Arn DEFAULT_ARN = Arn.builder().partition("aws").service("kinesis")
-            .region(KINESIS_REGION.toString()).accountId(TEST_ACCOUNT_ID).resource("stream/" + STREAM_NAME).build();
+    private static final Arn DEFAULT_ARN = toArn(KINESIS_REGION);
 
     @Before
     public void setUp() {
         mockStatic(StreamARNUtil.class);
 
-        when(StreamARNUtil.getStreamARN(anyString(), any(Region.class))).thenReturn(Optional.empty());
-        when(StreamARNUtil.getStreamARN(STREAM_NAME, KINESIS_REGION)).thenReturn(Optional.of(DEFAULT_ARN));
-        when(StreamARNUtil.getStreamARN(STREAM_NAME, KINESIS_REGION, Optional.of(TEST_ACCOUNT_ID)))
+        when(getStreamARN(anyString(), any(Region.class))).thenReturn(Optional.empty());
+        when(getStreamARN(STREAM_NAME, KINESIS_REGION)).thenReturn(Optional.of(DEFAULT_ARN));
+        when(getStreamARN(STREAM_NAME, KINESIS_REGION, Optional.of(TEST_ACCOUNT_ID)))
                 .thenReturn(Optional.of(DEFAULT_ARN));
+    }
+
+    /**
+     * Test patterns that should match a serialization regex.
+     */
+    @Test
+    public void testMultiStreamDeserializationSuccess() {
+        for (final String pattern : Arrays.asList(
+                // arn examples
+                toArn(KINESIS_REGION).toString(),
+                // serialization examples
+                "123456789012:stream-name:123",
+                "123456789012:stream-name:123:" + Region.US_ISOB_EAST_1
+        )) {
+            final StreamIdentifier si = StreamIdentifier.multiStreamInstance(pattern);
+            assertNotNull(si);
+        }
+    }
+
+    /**
+     * Test patterns that <b>should not</b> match a serialization regex.
+     */
+    @Test
+    public void testMultiStreamDeserializationFail() {
+        for (final String pattern : Arrays.asList(
+                // arn examples
+                "arn:aws:kinesis::123456789012:stream/stream-name", // missing region
+                "arn:aws:kinesis:region::stream/stream-name", // missing account id
+                "arn:aws:kinesis:region:123456789:stream/stream-name", // account id not 12 digits
+                "arn:aws:kinesis:region:123456789abc:stream/stream-name", // 12char alphanumeric account id
+                "arn:aws:kinesis:region:123456789012:stream/", // missing stream-name
+                // serialization examples
+                ":stream-name:123", // missing account id
+                "123456789012:stream-name", // missing delimiter before creation epoch
+                "accountId:stream-name:123", // non-numeric account id
+//                "123456789:stream-name:123", // account id not 12 digits
+                "123456789abc:stream-name:123", // 12char alphanumeric account id
+                "123456789012::123", // missing stream name
+                "123456789012:stream-name:", // missing creation epoch
+                "123456789012:stream-name::", // missing creation epoch; ':' for optional region yet missing region
+                "123456789012:stream-name::us-east-1", // missing creation epoch
+                "123456789012:stream-name:abc", // non-numeric creation epoch
+                "123456789012:stream-name:abc:", // non-numeric creation epoch with ':' yet missing region
+                "123456789012:stream-name:123:", // ':' for optional region yet missing region
+                ""
+        )) {
+            try {
+                StreamIdentifier.multiStreamInstance(pattern);
+                Assert.fail(pattern + " should not have created a StreamIdentifier");
+            } catch (final IllegalArgumentException iae) {
+                // expected; ignore
+            }
+        }
+    }
+
+    @Test
+    public void testInstanceFromArn() {
+        final Arn arn = toArn(KINESIS_REGION);
+        final StreamIdentifier single = StreamIdentifier.singleStreamInstance(arn.toString());
+        final StreamIdentifier multi = StreamIdentifier.multiStreamInstance(arn.toString());
+
+        assertEquals(single, multi);
+        assertEquals(Optional.of(TEST_ACCOUNT_ID), single.accountIdOptional());
+        assertEquals(STREAM_NAME, single.streamName());
+        assertEquals(Optional.of(arn), single.streamARNOptional());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testInstanceWithoutEpochOrArn() {
+        when(getStreamARN(STREAM_NAME, KINESIS_REGION, Optional.of(TEST_ACCOUNT_ID)))
+                .thenReturn(Optional.empty());
+
+        final Arn arn = toArn(KINESIS_REGION);
+        StreamIdentifier.singleStreamInstance(arn.toString());
     }
 
     @Test
@@ -54,44 +130,64 @@ public class StreamIdentifierTest {
         StreamIdentifier actualStreamIdentifier = StreamIdentifier.singleStreamInstance(STREAM_NAME, KINESIS_REGION);
         assertFalse(actualStreamIdentifier.streamCreationEpochOptional().isPresent());
         assertFalse(actualStreamIdentifier.accountIdOptional().isPresent());
-        assertTrue(actualStreamIdentifier.streamARNOptional().isPresent());
-        assertEquals(DEFAULT_ARN, actualStreamIdentifier.streamARNOptional().get());
+        assertEquals(Optional.of(DEFAULT_ARN), actualStreamIdentifier.streamARNOptional());
     }
 
     @Test
     public void testMultiStreamInstanceWithIdentifierSerialization() {
-        StreamIdentifier actualStreamIdentifier = StreamIdentifier.multiStreamInstance(
-                String.join(":", TEST_ACCOUNT_ID, STREAM_NAME, EPOCH, KINESIS_REGION.toString()));
-        assertActualStreamIdentifierExpected(DEFAULT_ARN, actualStreamIdentifier);
+        StreamIdentifier actualStreamIdentifier = StreamIdentifier.multiStreamInstance(serialize(KINESIS_REGION));
+        assertActualStreamIdentifierExpected(actualStreamIdentifier);
     }
 
     @Test
     public void testMultiStreamInstanceWithRegionSerialized() {
         Region serializedRegion = Region.US_GOV_EAST_1;
-        Optional<Arn> arn = Optional.ofNullable(Arn.builder().partition("aws").service("kinesis")
-                .accountId(TEST_ACCOUNT_ID).region(serializedRegion.toString()).resource("stream/" + STREAM_NAME).build());
+        final Optional<Arn> arn = Optional.of(toArn(serializedRegion));
 
-        when(StreamARNUtil.getStreamARN(eq(STREAM_NAME), eq(serializedRegion), any(Optional.class))).thenReturn(arn);
+        when(getStreamARN(STREAM_NAME, serializedRegion, Optional.of(TEST_ACCOUNT_ID))).thenReturn(arn);
+
+        final String expectedSerialization = serialize(serializedRegion);
         StreamIdentifier actualStreamIdentifier = StreamIdentifier.multiStreamInstance(
-                String.join(":", TEST_ACCOUNT_ID, STREAM_NAME, EPOCH, serializedRegion.toString()), KINESIS_REGION);
+                expectedSerialization, KINESIS_REGION);
         assertActualStreamIdentifierExpected(arn, actualStreamIdentifier);
+        assertEquals(expectedSerialization, actualStreamIdentifier.serialize());
+        verifyStatic(StreamARNUtil.class);
+        getStreamARN(STREAM_NAME, serializedRegion, Optional.of(TEST_ACCOUNT_ID));
     }
 
     @Test
     public void testMultiStreamInstanceWithoutRegionSerialized() {
         StreamIdentifier actualStreamIdentifier = StreamIdentifier.multiStreamInstance(
-                String.join(":", TEST_ACCOUNT_ID, STREAM_NAME, EPOCH), KINESIS_REGION);
-        assertActualStreamIdentifierExpected(DEFAULT_ARN, actualStreamIdentifier);
+                serialize(null), KINESIS_REGION);
+        assertActualStreamIdentifierExpected(actualStreamIdentifier);
     }
 
-    private void assertActualStreamIdentifierExpected(Optional<Arn> expectedARN, StreamIdentifier actual) {
-        assertActualStreamIdentifierExpected(expectedARN.get(), actual);
+    private void assertActualStreamIdentifierExpected(StreamIdentifier actual) {
+        assertActualStreamIdentifierExpected(Optional.of(DEFAULT_ARN), actual);
     }
 
-    private void assertActualStreamIdentifierExpected(Arn expectedARN, StreamIdentifier actual) {
-        assertTrue(actual.streamCreationEpochOptional().isPresent());
-        assertTrue(actual.accountIdOptional().isPresent());
-        assertTrue(actual.streamARNOptional().isPresent());
-        assertEquals(Optional.of(expectedARN), actual.streamARNOptional());
+    private void assertActualStreamIdentifierExpected(Optional<Arn> expectedArn, StreamIdentifier actual) {
+        assertEquals(STREAM_NAME, actual.streamName());
+        assertEquals(Optional.of(EPOCH), actual.streamCreationEpochOptional());
+        assertEquals(Optional.of(TEST_ACCOUNT_ID), actual.accountIdOptional());
+        assertEquals(expectedArn, actual.streamARNOptional());
+    }
+
+    /**
+     * Creates a pattern that matches {@link StreamIdentifier} serialization.
+     *
+     * @param region (optional) region to serialize
+     */
+    private static String serialize(final Region region) {
+        return String.join(":", TEST_ACCOUNT_ID, STREAM_NAME, Long.toString(EPOCH)) +
+                ((region == null) ? "" : ':' + region.toString());
+    }
+
+    private static Arn toArn(final Region region) {
+        return Arn.builder().partition("aws").service("kinesis")
+                .accountId(TEST_ACCOUNT_ID)
+                .resource("stream/" + STREAM_NAME)
+                .region(region.toString())
+                .build();
     }
 }
