ee3f56ae660ae86a4d48c4953eb48afb7e2628b5
stair <123031771+stair-aws@users.noreply.github.com>
2023-05-05 14:29:34 -0400

    Added missing copyright to new files. (#1106)

--- amazon-kinesis-client/src/main/java/software/amazon/kinesis/common/SupplierCache.java
+++ amazon-kinesis-client/src/main/java/software/amazon/kinesis/common/SupplierCache.java
@@ -1,3 +1,17 @@
+/*
+ * Copyright 2023 Amazon.com, Inc. or its affiliates.
+ * Licensed under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package software.amazon.kinesis.common;
 
 import java.util.function.Supplier;
@@ -7,6 +21,8 @@ import lombok.RequiredArgsConstructor;
 /**
  * Caches results from a {@link Supplier}. Caching is especially useful when
  * {@link Supplier#get()} is an expensive call that produces static results.
+ *
+ * @param <T> result type
  */
 @RequiredArgsConstructor
 public class SupplierCache<T> extends SynchronizedCache<T> {
