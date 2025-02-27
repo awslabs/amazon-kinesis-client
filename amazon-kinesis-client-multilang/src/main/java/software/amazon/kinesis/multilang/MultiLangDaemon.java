995325813bac5d452589c61f6e229d0e28a20183
Vincent Viloria <vvilo@amazon.com>
2025-02-27 15:10:01 -0800

    update docs for credential provider

--- amazon-kinesis-client-multilang/src/main/java/software/amazon/kinesis/multilang/MultiLangDaemon.java
+++ amazon-kinesis-client-multilang/src/main/java/software/amazon/kinesis/multilang/MultiLangDaemon.java
@@ -61,7 +61,7 @@ import software.amazon.kinesis.coordinator.Scheduler;
  * applicationName = PythonKCLSample
  *
  * # Users can change the credentials provider the KCL will use to retrieve credentials.
- * # The DefaultAWSCredentialsProviderChain checks several other providers, which is
+ * # The DefaultCredentialsProvider checks several other providers, which is
  * # described here:
  * # https://sdk.amazonaws.com/java/api/2.0.0-preview-11/software/amazon/awssdk/auth/credentials/DefaultCredentialsProvider.html
  * AwsCredentialsProvider = DefaultCredentialsProvider
