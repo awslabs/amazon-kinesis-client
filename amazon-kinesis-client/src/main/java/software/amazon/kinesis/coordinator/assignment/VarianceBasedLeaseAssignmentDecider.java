850a6b9bce6a15de7e064d446cbc8856f81e2266
eha sah <ehasah@amazon.com>
2026-09-21 19:46:20 -0700

    Clamp negative lower threshold to 0 in rebalance log output

--- amazon-kinesis-client/src/main/java/software/amazon/kinesis/coordinator/assignment/VarianceBasedLeaseAssignmentDecider.java
+++ amazon-kinesis-client/src/main/java/software/amazon/kinesis/coordinator/assignment/VarianceBasedLeaseAssignmentDecider.java
@@ -146,7 +146,7 @@ public final class VarianceBasedLeaseAssignmentDecider implements LeaseAssignmen
 
         WorkerMetricStats mostLoadedWorker = null;
 
-        log.info("Range for re-balance upper threshold {} and lower threshold {}", upperLimit, lowerLimit);
+        log.info("Range for re-balance upper threshold {} and lower threshold {}", upperLimit, Math.max(0, lowerLimit));
 
         boolean shouldTriggerReBalance = false;
         for (final WorkerMetricStats workerMetrics : currentWorkerMetrics) {
